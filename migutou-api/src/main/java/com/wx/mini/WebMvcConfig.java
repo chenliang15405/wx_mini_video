2ac9b2687aca2e2553587389f22977f92f4b747a
chenliang15405 <1165243776@qq.com>
2019-09-15 17:58:15 +0800

    feat: 增加interceptor校验登录token

--- migutou-api/src/main/java/com/wx/mini/WebMvcConfig.java
+++ migutou-api/src/main/java/com/wx/mini/WebMvcConfig.java
@@ -1,7 +1,10 @@
 package com.wx.mini;
 
+import com.wx.mini.interceptor.MiniInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -17,6 +20,8 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Value("${user.upload.face.fileSpace}")
     private String fileSpace;
 
+    @Autowired
+    private MiniInterceptor miniInterceptor;
 
     /**
      * 添加资源路径，可以直接访问对应的静态资源,例如 图片、html等
@@ -27,5 +32,13 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/META-INF/resources/")// 设置swagger-ui.html可以访问
                 .addResourceLocations("file:" + fileSpace + File.separator);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(miniInterceptor)
+                .addPathPatterns("/user/**")
+                .addPathPatterns("/bgm/**")
+                .addPathPatterns("/video/upload","/video/uploadCover");
     }
 }
---
+++ migutou-api/src/main/java/com/wx/mini/interceptor/MiniInterceptor.java
@@ -0,0 +1,95 @@
+package com.wx.mini.interceptor;
+
+import com.alibaba.druid.support.json.JSONUtils;
+import com.wx.mini.utils.IMoocJSONResult;
+import com.wx.mini.utils.JsonUtils;
+import com.wx.mini.utils.RedisOperator;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * 用户登录拦截器
+ *
+ * @auther alan.chen
+ * @time 2019/9/15 4:33 PM
+ */
+@Slf4j
+@Component
+public class MiniInterceptor implements HandlerInterceptor {
+
+
+    @Autowired
+    private RedisOperator redisOperator;
+
+    private static final String REDIS_USER_SESSION_TOKEN = "user-session-token";
+
+
+
+    /**
+     * 判断用户是否登录
+     * @param httpServletRequest
+     * @param httpServletResponse
+     * @param o
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        String userId = httpServletRequest.getHeader("userId");
+        String token = httpServletRequest.getHeader("userToken");
+        log.info("interceptor userId: {}  token: {}", userId, token);
+        if(StringUtils.isBlank(userId) || StringUtils.isBlank(token)) {
+            msgResponse(httpServletResponse, IMoocJSONResult.errorMsg("请登录"));
+            return false;
+        }
+        String redisToken = redisOperator.get(REDIS_USER_SESSION_TOKEN + ":" + userId);
+        log.info("MiniInterceptor preHandle....  token: ", redisToken);
+        if(StringUtils.isBlank(redisToken)) {
+            msgResponse(httpServletResponse, IMoocJSONResult.errorTokenMsg("请登录"));
+            return false;
+        }
+        if(!redisToken.equals(token)) {
+            msgResponse(httpServletResponse, IMoocJSONResult.errorTokenMsg("登录已过期，请重新登录"));
+            return false;
+        }
+        return true;
+    }
+
+    public void msgResponse(HttpServletResponse response, IMoocJSONResult result) {
+        OutputStream out = null;
+
+        try {
+            response.setContentType("application/json; charset=utf-8");
+            response.setCharacterEncoding("UTF-8");
+            out = response.getOutputStream();
+            out.write(JsonUtils.objectToJson(result).getBytes("UTF-8"));
+            out.flush();
+        } catch (IOException e) {
+            log.error("msgResponse interceptor error ", e);
+        } finally {
+            IOUtils.closeQuietly(out);
+        }
+    }
+
+
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
